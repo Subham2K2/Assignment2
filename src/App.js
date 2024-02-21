2e703451cc2277d86d04b83c6e3850310b007124
Subham2K2 <90100150+Subham2K2@users.noreply.github.com>
2024-02-21 20:00:34 +0530

    Add Image functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  // Dragging Effect
+
+
+
+  const onSelectFile = (event) => {
+    const selectedFiles = event.target.files;
+    // console.log(selectedFiles);
+    // console.log(Array.isArray(selectedFiles))
+    const selectedFilesArray = Array.from(selectedFiles);
+
+    // console.log(selectedFilesArray)
+
+    const imagesArray = selectedFilesArray.map((File) => {
+      return URL.createObjectURL(File);
+    });
+
+    // setSelectedImages(imagesArray);
+    setSelectedImages((previousImages) => previousImages.concat(imagesArray));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <label>
+        + Add Images
+        <br />
+        <input type='file' name='images' onChange={onSelectFile} multiple accept='image/png, image/jpeg, image/webp' />
+      </label>
+      <div className='images'>
+        {selectedImages && selectedImages.map((image, index) => {
+          return (
+            <div key={image} className='image'>
+              <img key={index} src={image} className='image' draggable />
+            </div>
+          )
+        })}
+      </div>
+    </section>
   );
 }
 
 export default App;
+
+
+{/* <button onClick={() => setSelectedImages(selectedImages.filter((e) => e !== image))}>
+                delete image
+              </button> */}
